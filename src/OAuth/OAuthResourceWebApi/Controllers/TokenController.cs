217d7ec5657483b15b1a22f74881146588e14d50
Henry <huyonghui@moneyking.cn>
2014-05-07 17:12:21 +0800

    Resource webapi

---
+++ src/OAuth/OAuthResourceWebApi/Controllers/TokenController.cs
@@ -0,0 +1,21 @@
+﻿using DotNetOpenAuth.OAuth2;
+using OAuthResourceWebApi.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OAuthResourceWebApi.Controllers
+{
+    public class TokenController : ApiController
+    {
+        public Task<HttpResponseMessage> Post(HttpRequestMessage request)
+        {
+            var authServer = new AuthorizationServer(new AuthorizationServerHost());
+            return authServer.HandleTokenRequestAsync(request);
+        }
+    }
+}
---
+++ src/OAuth/OAuthResourceWebApi/Infrastructure/BearerTokenHandler.cs
@@ -0,0 +1,30 @@
+﻿using DotNetOpenAuth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OAuthResourceWebApi.Infrastructure
+{
+    public class BearerTokenHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Headers.Authorization != null)
+            {
+                if (request.Headers.Authorization.Scheme == "Bearer")
+                {
+                    var resourceServer = new ResourceServer(new StandardAccessTokenAnalyzer(AuthorizationServerHost.HardCodedCryptoKeyStore));
+                    var principal = await resourceServer.GetPrincipalAsync(request, cancellationToken);
+                    HttpContext.Current.User = principal;
+                    Thread.CurrentPrincipal = principal;
+                }
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
